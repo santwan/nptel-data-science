52a5a46e08158c58dbf3fd54e18f50cd7b61aec2
santwan <santwanpathak1@gmail.com>
2025-03-20 23:47:40 +0530

    Update casestudy.py

--- casestudy.py
+++ casestudy.py
@@ -71,7 +71,7 @@ data['occupation'].value_counts()
 
 
 #*** Checking for unique classes
-#print(np.unique(data['JobType']))
+print(np.unique(data['JobType']))
 print(np.unique(data['occupation']))
 
 
@@ -82,14 +82,63 @@ Go back and read teh data by including "na_values['?']" to consider '?' as NaN
 
 """
 data = pd.read_csv('income.csv', na_values=[" ?"])
-print(np.unique(data['JobType']))
+
+#=============================================
+#Data pre-processing 
+#=============================================
+
+data.isnull().sum()
 
 
+missing = data[data.isnull().any(axis=1)]
+#axis=1 means to consider at least one column value is missing 
+
+"""
+Points to Note:
+    1. Missing values in Jotype = 1809
+    2. Missing values in Occupation = 1816
+    3. There are 1809 rows where two specific column 
+    that is occupatio  and jobtype have missing values
+    4. 1816 - 1809 =  7 => You still have occupation unfilled for 
+    these 7 rows, because, jobtype is never worked
+"""
+
+data2 = data.dropna(axis = 0)
+#dropping all the rows in the dataset where the NaN or null value is present 
+
+#Relationship between independent variable
+#correlation =  data2.corr()
+correlation = data2.select_dtypes(include=['number']).corr()
+#print(correlation)
+#None of the numerical variables are corelated
 
 
+#=============================================================================
+#Cross Tables and Data Visualization
+#===============================================================================
+# Extracting the columns name
+data2.columns
+print(data2.dtypes)
 
 
+#===========================
+#Gender proportion table:
+    
+gender = pd.crosstab( index = data2["gender"], columns = 'count', normalize = True )
+
+print(gender)
 
 
+#===========================
+#Gender vs salary status:
+    
+gender_salstat = pd.crosstab(index = data2["gender"], columns = data2["SalStat"], margins = True, normalize= "index")
 
+print(gender_salstat)
+
+
+#===========================
+# Frequency Distribution of 'Salary status'
+
+salstat = sns.countplot(data2['SalStat'])
 
