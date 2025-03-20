c5e219b6a045d1703868c2dc1b9e438894b2d457
santwan <santwanpathak1@gmail.com>
2025-03-20 22:35:15 +0530

    Update casestudy.py

--- casestudy.py
+++ casestudy.py
@@ -39,8 +39,50 @@ data_income = pd.read_csv('income.csv')
 data = data_income.copy()
 
 
+"""
+#Exploratory data analysis
+
+#1. Getting to know the data 
+#2. Data preprocessing (missing values)
+#3. Cross table and data visualization
+"""
+#===============================================================
+#Getting to know the data
+#===========================================================================================
+#****** to check the variables' data type
+print(data.info())
+
+#***** To Check for missing values
+data.isnull()
+
+print("Data columns with null values:/n", data.isnull().sum())
+
+#******* Summary of numerical variables
+summary_num = data.describe()
+print(summary_num)
+
+#***** Summary of catagorical variable(object type)
+summary_cate = data.describe(include = "O")
+print(summary_cate)
+
+#**********   Frequency of each categories
+data['JobType'].value_counts()  #it gives how many types of job are in there and frequency of each job occurence 
+data['occupation'].value_counts()
 
 
+#*** Checking for unique classes
+#print(np.unique(data['JobType']))
+print(np.unique(data['occupation']))
+
+
+#******* There exists '?' instead of sum
+
+""" 
+Go back and read teh data by including "na_values['?']" to consider '?' as NaN
+
+"""
+data = pd.read_csv('income.csv', na_values=[" ?"])
+print(np.unique(data['JobType']))
 
 
 
